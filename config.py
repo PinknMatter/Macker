7d2b994e3eda038aee47fb7dd14d2f1a8a470bd4
noah kornberg <94400146+PinknMatter@users.noreply.github.com>
2024-12-02 23:51:05 -0500

    fixed creds

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 NOTIFICATION_EMAIL = "noahkornberg@gmail.com"  ## This is the email address that will receive notifications
-CREDENTIALS_SHEET = "macker-428721-78ccb1f3325e.json"  ## This is the file that contains the credentials for the Google Sheets API
-CREDENTIALS_GMAIL = "credentials2.json"  ## This is the GMAIL credentials JSON
+CREDENTIALS_SHEET = "./Creds/ServiceKey.json"  ## This is the file that contains the credentials for the Google Sheets API
+CREDENTIALS_GMAIL = "./Creds/sheetscred.json"  ## This is the GMAIL credentials JSON
 SPREAD_SHEET = "Macker"  ## This is the spreadsheet name to store updates
